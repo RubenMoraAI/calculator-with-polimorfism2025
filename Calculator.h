ea3968246a59f6bb3c49d94b4ac52638dbf0bd23
Rubén Mora Vargas <rubenmoravargas@gmail.com>
2025-08-27 15:18:07 -0600

    Create Calculator.h
    Add Calculator.h file

---
+++ Calculator.h
@@ -0,0 +1,10 @@
+class Calculator {
+protected:
+    double result; // solo lectura
+public:
+    Calculator() : result(0) {}
+    virtual void calculate() = 0;    // método abstracto
+    virtual void print() const = 0;  // método abstracto
+    double getResult() const { return result; }
+    virtual ~Calculator() {}
+};
