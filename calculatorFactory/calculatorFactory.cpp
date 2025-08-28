24a0703da90d7c9223823e9cc96b9119f6579e9b
Rubén Mora Vargas <rubenmoravargas@gmail.com>
2025-08-27 19:55:28 -0600

    Add Basic Calculator as sample

--- calculatorFactory/calculatorFactory.cpp
+++ calculatorFactory/calculatorFactory.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Calculator.h"
+#include "BasicCalculator.h"
 using namespace std;
 
 int main() {
     Calculator* calcs[10];
     int count = 0;
-
+	calcs[count++] = new BasicCalculator(5, 3);
+     
     // =============================
     // Aquí cada estudiante agregará su calculadora
     // Ejemplo (cuando tengas tu clase):
